5e1d0e6ca3ba46873fe68bb2abab637c25288ae2
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-02-07 13:39:54 -0300

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,17 @@
-const { Configuration, OpenAIApi } = require("openai");
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const configuration = new Configuration({
-    organization: "org-6ZFQ3THbfBvZay2O1lt27syk",
-    apiKey: // your_API_key,
-});
-const openai = new OpenAIApi(configuration);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
-
-
-const port = 3080
-
-app.post('/', async (req, res) => {
-    const { message } = req.body;
-    console.log(message)
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${message}`,
-        max_tokens: 100,
-        temperature: 0.5,
-      });
-    res.json({
-        message: response.data.choices[0].text,
-    })
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
